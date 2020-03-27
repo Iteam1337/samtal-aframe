ebe145e5d03546c76f4789b8b4a5cc4fe3c4d727
Nils Nilsson <nilspontusnilsson@gmail.com>
2020-03-27 12:09:00 +0100

    feat: add facemesh and sockets

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const path = require('path')
+const port = process.env.PORT || 8000
+
+server.listen(port)
+console.log('listening on port ', port)
+
+const allFaces = {}
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/index.html'))
+)
+
+io.on('connection', (client) => {
+  client.on('face', (face) => {
+    allFaces[client.id] = face
+  })
+
+  setInterval(() => {
+    io.emit(
+      'faces',
+      Object.values(allFaces).reduce(
+        (result, faces) => result.concat(faces),
+        []
+      )
+    )
+  }, 10)
+
+  client.on('disconnect', (reason) => {
+    delete allFaces[client.id]
+  })
+})
