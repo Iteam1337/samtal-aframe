fde856474d9c8dbc180d0f21add385d3c43d95a0
Rickard Laurin <rickard.laurin@gmail.com>
2020-04-02 10:12:43 +0200

    fix: mouth width

--- src/script.js
+++ src/script.js
@@ -43,11 +43,12 @@ const detectFace = async (model, video, emitFace) => {
             getPositions(annotations.lipsLowerOuter[4])
           ).y,
           width: diff(
-            getPositions(annotations.lipsLowerOuter[0]),
+            getPositions(annotations.lipsUpperOuter[0]),
             getPositions(annotations.lipsLowerOuter[0])
           ).x,
         },
       }
+
       emitFace(strippedFace)
     })
 
