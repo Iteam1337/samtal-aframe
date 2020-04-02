0e92a2295bc8bf59afaced3d7e2aaa68d8f790f0
Rickard Laurin <rickard.laurin@gmail.com>
2020-04-02 14:17:07 +0200

    feat: basic sound implementation

--- src/sounds.js
+++ src/sounds.js
@@ -1,6 +1,16 @@
 const audio = document.getElementById('happy')
+const socket = io('localhost:8000')
+
+let soundReady = false
 
 audio.addEventListener('canplay', () => {
-  console.log('ready to play sound')
-  //audio.play()
+  soundReady = true
+})
+
+socket.on('faces', (faces) => {
+  faces.map((face) => {
+    if (face.mouth.height * 10 > 0.6) {
+      audio.play()
+    }
+  })
 })
