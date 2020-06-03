47d154f4a17048d415b30820253a66922042217f
Christian Landgren <christian@landgren.nu>
2020-06-03 23:11:11 +0200

    now streaming works

--- src/jitsi.js
+++ src/jitsi.js
@@ -17,20 +17,21 @@ export async function startJitsi (roomName, videoElement, presentationElement) {
 
   const customConfig = {
     hosts: {
-      domain: 'jitsi.vrmeet.io',
-      muc: 'jitsi.vrmeet.io'
+      domain: 'meet.jitsi',
+      muc: 'muc.meet.jitsi'
     },
-    //bosh: 'https://jitsi.vrmeet.io/http-bind',
-    serviceUrl: 'https://jitsi.vrmeet.io/http-bind',
+    bosh: 'https://jitsi.vrmeet.io/http-bind',
+    //serviceUrl: 'wss://jitsi.vrmeet.io/xmpp-websocket',
+    //serviceUrl: `https://jitsi.vrmeet.io/http-bind?room=${roomName}`,
     //websocket: 'wss://jitsi.vrmeet.io/xmpp-websocket',
     //serviceUrl: 'wss://meet.jit.si/xmpp-websocket',
 
-    clientNode: 'https://jitsi.vrmeet.io',
+    clientNode: 'https://jitsi.org/jitsimeet',
   };
 
 
 
-  const connection = new JitsiMeetJS.JitsiConnection(null, null, config)
+  const connection = new JitsiMeetJS.JitsiConnection(null, null, customConfig)
   connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionEstablished)
 
   await connection.connect()
@@ -43,14 +44,14 @@ export async function startJitsi (roomName, videoElement, presentationElement) {
     room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onTrackAdded)
       room.join()
       JitsiMeetJS.createLocalTracks({ devices: [ 'audio', 'video' ] }).then(localTracks =>{
-      // localTracks.forEach(track => room.addTrack(track))
+      localTracks.forEach(track => room.addTrack(track)) // remove this to send video
     })
   }
 
   function onTrackAdded(track) {
     console.log('track added, remote?', !track.isLocal())
 
-   
+    if (track.isLocal()) return // skip local tracks, we are only interested in the presenters view
     
     if (track.getType() === 'video') {
       const video = track.isLocal() ? videoElement : presentationElement
